64a555eec51a0b7842556755dde07e1043a19008
yuriy.kh <yuriy.kh@siteplus.com>
2022-08-23 02:12:47 +0300

    added clone props and empty array props for useMemoDeep and useCallbackDeep

--- src/index.js
+++ src/index.js
@@ -1,23 +1,41 @@
 'use strict';
 
-const { useRef, memo } = require('react');
+const {
+  qcloneCircular
+} = require('qclone');
+const {
+  useRef,
+  memo
+} = require('react');
 const compareDeep = require('./utils/compareDeep');
 const memoizeDeep = require('./utils/memoizeDeep');
 
 const memoDeep = (component) => memo(component, compareDeep);
 
-function useMemoDeep(func, props) {
+function useMemoDeep(func, props, isCloneProps = false) {
   const refInputs = useRef(null);
+  if (props.length === 0 && refInputs.current) {
+    return refInputs.current.res;
+  }
   if (!refInputs.current || !compareDeep(refInputs.current.props, props)) {
-    refInputs.current = { res: func(), props };
+    refInputs.current = {
+      res: func(),
+      props: isCloneProps ? qcloneCircular(props) : props
+    };
   }
   return refInputs.current.res;
 }
 
-function useCallbackDeep(func, props) {
+function useCallbackDeep(func, props, isCloneProps = false) {
   const refInputs = useRef(null);
+  if (props.length === 0 && refInputs.current) {
+    return refInputs.current.res;
+  }
   if (!refInputs.current || !compareDeep(refInputs.current.props, props)) {
-    refInputs.current = { res: func, props };
+    refInputs.current = {
+      res: func,
+      props: isCloneProps ? qcloneCircular(props) : props
+    };
   }
   return refInputs.current.res;
 }
